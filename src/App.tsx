bbd440d0cb1a48549c321041b06066f38130f4b4
Anuj Singla <asingla@redhat.com>
2022-08-04 11:46:50 +0530

    initial project code push .

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+// import logo from "./logo.svg";
+import "@patternfly/react-core/dist/styles/base.css";
+import "./styles/App.scss";
+// import "./App.css";
+import {
+  Nav,
+  NavItem,
+  NavList,
+  Page,
+  // PageSection,
+  PageSidebar,
+} from "@patternfly/react-core";
 
 function App() {
+  const [activeItem, setActiveItem] = useState(0);
+  const onChangeItem = (result: any) => {
+    setActiveItem(result.itemId);
+  };
+  const PageNav = (
+    <Nav onSelect={onChangeItem} aria-label="Nav">
+      <NavList>
+        <NavItem itemId={0} isActive={activeItem === 0} to="#system-panel">
+          System panel
+        </NavItem>
+        <NavItem itemId={1} isActive={activeItem === 1} to="#policy">
+          Policy
+        </NavItem>
+        <NavItem itemId={2} isActive={activeItem === 2} to="#auth">
+          Authentication
+        </NavItem>
+        <NavItem itemId={3} isActive={activeItem === 3} to="#network">
+          Network services
+        </NavItem>
+        <NavItem itemId={4} isActive={activeItem === 4} to="#server">
+          Server
+        </NavItem>
+      </NavList>
+    </Nav>
+  );
+  const _sidebar = <PageSidebar nav={PageNav} isNavOpen={true} />;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Page
+      header={null}
+      sidebar={_sidebar}
+      isManagedSidebar
+      mainContainerId={"main-content-page-layout-default-nav"}
+      // onPageResize={onPageResize}
+    >
+      {/* {hasPageTemplateTitle && PageTemplateTitle} */}
+      <p>Test content</p>
+    </Page>
   );
 }
 
